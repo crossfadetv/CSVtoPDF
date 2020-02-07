ad3adbd760557185fa4adfc64636eb51d9b5ef8c
markusteiner <markusteiner@gmail.com>
2020-02-07 12:44:19 +0100

    commit to merge

--- src/CSVtoPDF.java
+++ src/CSVtoPDF.java
@@ -1,10 +1,23 @@
 import ch.kidin.util.CSVReader;
+import ch.kidin.util.GeneratePDF;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CSVtoPDF {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        CSVReader result = new CSVReader(18);
+        HashMap<String, Double[]> result = new CSVReader(18).getAmountPerFamily();
 
+        for (Map.Entry<String,Double[]> entry : result.entrySet()) {
+            try {
+                GeneratePDF pdf = new GeneratePDF(entry.getKey());
+                pdf.writePDF();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/ch/kidin/util/GeneratePDF.java
+++ src/ch/kidin/util/GeneratePDF.java
@@ -1,4 +1,68 @@
 package ch.kidin.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
 public class GeneratePDF {
+
+    private final String PATH = "D:\\test\\";
+    private String fileName;
+    private String fullPath;
+
+    public GeneratePDF(String fileName) {
+        this.fileName = fileName;
+        fullPath = PATH + fileName + ".pdf";
+    }
+
+    public void writePDF() throws IOException {
+
+        //create doc
+        PDDocument document = new PDDocument();
+
+        //create page
+        PDPage page = new PDPage();
+
+        //add page to doc
+        document.addPage(page);
+
+        //add content
+
+
+        PDPageContentStream layout = new PDPageContentStream(document,page);
+
+        //add header
+        layout.beginText();
+        layout.setFont(PDType1Font.HELVETICA, 14);
+        layout.newLineAtOffset(100, 750);
+        layout.showText("Kinderwelt kidin.ch | Schulweg 1 | 8616 Riedikon | info@kidin.ch");
+        layout.endText();
+
+
+        //add title
+        layout.beginText();
+        layout.setFont(PDType1Font.COURIER_BOLD,26);
+        layout.newLineAtOffset(200,600);
+        layout.showText("Steuernachweis");
+        layout.endText();
+
+        //add logo
+        PDImageXObject logoImage = PDImageXObject.createFromFile("C:\\Users\\marku\\Desktop\\kidin.png",document);
+        layout.drawImage(logoImage,400,700,50,50);
+
+        layout.close();
+
+
+
+        //save doc
+        document.save(fullPath);
+
+        //cleaning memory
+        document.close();
+        System.out.println("Document: " + fileName + ".pdf was generated in " + PATH);
+    }
 }
